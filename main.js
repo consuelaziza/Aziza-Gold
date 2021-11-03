a897bb40adad0974f95008381bf7f91902436ca7
Consuela Vlek <consuelavlek@Consuelas-MacBook-Air.local>
2021-11-03 22:24:17 +0100

    had help  with the front page

--- main.js
+++ main.js
@@ -7,40 +7,50 @@ let startPage = document.querySelector("#startPage");
 
 let bg = new Image();
 bg.src = "./images/bg.png";
+
 let fg = new Image();
 fg.src = "./images/fg.png";
+
 let fairy = new Image();
 fairy.src = "./images/aziza queen.png";
+
 let star = new Image();
 star.src = "./images/star.png";
+
 let tree = new Image();
 tree.src = "./images/tree.png";
+
 let gold = new Image();
 gold.src = "./images/gold dust.png";
+
 let hunter = new Image();
 hunter.src = "./images/hunter.png";
+
 let intervalId = 0;
 let isGameOver = false;
-let fairyY = 30,
-  fairyX = 30;
-let starsX = 500;
+let fairyY = 30, fairyX = 30;
+let starsX = 200;
 let treesX = 200;
-let treesY = 500;
+let treesY = 400;
 let decTrees = 2;
 let score = 0;
 let falling = true;
+
 let stars = [
   { x: starsX, y: 0 },
   { x: starsX + 300, y: -100 }
 ];
+
 let trees = [
   { x: treesX, y: 0 },
   { x: treesX + 300, y: -100 }
 ];
+
 function showGameOver() {
   canvas.style.display = "none";
   restartBtn.style.display = "block";
 }
+
 function draw() {
   ctx.drawImage(bg, 0, 0);
   ctx.drawImage(fairy, fairyX, fairyY);
@@ -94,22 +104,26 @@ function draw() {
   }
   ctx.font = "24px Helvetica";
   ctx.fillText(`Score: ${score}`, 10, 30);
+
   if (isGameOver) {
     cancelAnimationFrame(intervalId);
   } else {
     intervalId = requestAnimationFrame(draw);
   }
 }
+
 function handleStart() {
   draw();
   startBtn.style.display = "none";
+  startPage.style.display = "none";
   restartBtn.style.display = "none";
   canvas.style.display = "block";
 }
+
 window.addEventListener("load", () => {
   canvas.style.display = "none";
   restartBtn.style.display = "none";
-  startPage.style.display = "none";
+  startPage.style.display = "static";
   document.addEventListener("mousedown", () => {
     falling = false;
   });
